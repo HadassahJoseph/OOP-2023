5566e56a6e1458352ce313e6ed60a97268ed08cf
Hadassah <105879604+MAYRIAM@users.noreply.github.com>
2023-01-30 15:37:14 +0000

    all seeing eye

---
+++ java/src/ie/tudublin/Cat.java
@@ -0,0 +1,15 @@
+package ie.tudublin;
+
+public class Cat {
+    String name;
+    private int numlives;
+
+    public Cat(String name)
+    {
+        this.name = name;
+        numlives = 9;
+    }
+
+    
+    
+}
--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -4,64 +4,70 @@ import processing.core.PApplet;
 
 public class HelloProcessing extends PApplet
 {
-
+// class that extends or inhertices another class therefore this class gets al the methods that the 
+//pApplet class in java it called supersharp
 	public void settings()
 	{
 		size(500, 500);
+		//fullScreen();
 	}
 
 	public void setup() {
-		colorMode(HSB);
-		background(0);
-
-		x1 = random(0, width);
-		x2 = random(0, width);
-		y1 = random(0, height);
-		y2 = random(0, height);
-
-		float range = 5;
-
-		x1dir = random(-range, range);
-		x2dir = random(-range, range);
-		y1dir = random(-range, range);
-		y2dir = random(-range, range);
-
-		smooth();
-		
+		//colorMode(HSB);
+		//hsb - brightness , muche asier to work with , queue staruiation brightness
+		background(255,0, 0);
+		// this get called right at the strat along with setiings 
 	}
 
-	float x1, y1, x2, y2;
-	float x1dir, x2dir, y1dir, y2dir;
-	float c = 0;
+	
 	
 	public void draw()
 	{	
-		strokeWeight(2);
-		stroke(c, 255, 255);
-		c = (c + 1f) % 255;
-		line(x1, y1, x2, y2);
-
-		x1 += x1dir;
-		x2 += x2dir;
-		y1 += y1dir;
-		y2 += y2dir;
+		// draw gets called at every frame i.e film is a series of series of still images 
+		//background(0);
+		//noStroke();
+		//fill(mouseX, 255, 255);;
+		//circle(random(0, width), random (0, width), random(0,width));
+		fill(255,255,0);
+		circle(250, 250, 400);
+		noStroke();
+		//triangle(50,450,450,450,50,450); 
+		fill(0, 0, 255);
+		//triangle(300, 300, 900, 900, 500, 500); 
+		triangle(5,400,500,400,245,20);
+		//triangle(55,300,440,300,250,50);
+		//triangle(300, 40, 90, 20, 80, 70);
+		fill(255,255,255);
+		ellipse(250, 250, 200,80 );//x1,y1,w,h
 		
-		if (x1 < 0 || x1 > width)
-		{
-			x1dir = - x1dir;
-		}
-		if (y1 < 0 || y1 > height)
-		{
-			y1dir = - y1dir;
+		int pupilSize = 100;
+		if (mouseX > 250 && mouseY > 250) {
+		  pupilSize = 50;
 		}
 
-		if (x2 < 0 || x2 > width)
-		{
-			x2dir = - x2dir;
-		}
-		if (y2 < 0 || y2 > height)
-		{
-			y2dir = - y2dir;
-		}
+		fill(0,0,0);
+		ellipse(250, 250, pupilSize, pupilSize);
+		
+		
+		
+
+		
+
+		
+		/*stroke(255);
+		line(10, 10, 100, 100); //x1, y1 , x2, y2
+		circle(300, 250, 70); //cx, cy, d
+		fill(0, 255, 0);
+		rect(10,300,10,100); // tlx, tly, w, h
+		stroke(127);
+		fill(0, 0, 255);
+		noStroke();
+		noFill();
+		strokeWeight(1);
+		triangle(300, 300, 900, 900, 500, 500); // (x1, y1), (x2, y2), and (x3, y3) 
+		fill(0,0, 255);*/
+		
+	
+
 	}
 }
