906296594773cc4142446655530f80384b33037b
Hadassah <105879604+MAYRIAM@users.noreply.github.com>
2023-02-08 10:35:18 +0000

    finding my way

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -5,63 +5,78 @@ import processing.core.PApplet;
 public class BugZap extends PApplet
 {
 
+	float playerX ,playerY, playerWidth;
+	float bugX, bugY, bugWidth;
+	float halfPlayer , halfBug;
+
+
+
 	public void settings()
 	{
 		size(500, 500);
 	}
 	
-	float playerX;
-	float playerY;
-	float playerWidth;
+	
 
 	public void setup() {
-		//background (0);
 		colorMode(HSB);
-		//background (0);
-
+		background(0);
 		smooth();
 
-		playerX = width /2 ;
-		playerY = height - 50;
+
+		playerX = width * 0.5f;// converts to float point constant 
+		playerY = height - 100; //height of screen  	
+	
 		playerWidth = 50;
+		halfPlayer = playerWidth * 0.5f;
+	}
+
+	void restBug() {
+		bugY = 50;
+		bugWidth = 50;
+		halfBug = bugWidth * 0.5f ;
+		bugX = random(halfBug, width - halfBug);
 	}
 
 	void drawPlayer(float x, float y, float w )
 	{
-		stroke (255);
 		noFill();
+		stroke(255);
 		rectMode(CENTER);
-		rect(x, y, w, 20);
-		//size(100, 10);
-		//stroke(255,0 ,0 );
-		line(x, y, 10, x, y, 20);
-		//size(400, 400);
-		//line(120, 80, 340, 300);
+		rect(x, y, w, w);
+		line (x, y  - halfPlayer, x , y - halfPlayer * 2);
+
 	}
 
-	public void draw() {
-		background (0);
-		strokeWeight(2);
-		drawPlayer (playerX,playerY,playerWidth);
-	}
-
-	/*public void keyPressed()
+	public void keyPressed() // public - makes method visible outside every-other class
 	{
+		//POLYMORISHEM :-  many shap :- it is a concept meaning thing can change cat van turn to a dog 
+		// the type is of a superclass but the intance is of the subclass
 		if (keyCode == LEFT)
 		{
 			System.out.println("Left arrow pressed");
-			playerX -- ;
+			playerX --;
 			
 		}
 		if (keyCode == RIGHT)
 		{
 			System.out.println("Right arrow pressed");
-			playerX ++ ;
+			playerX ++;
+	
 		}
 		if (key == ' ')
 		{
 			System.out.println("SPACE key pressed");
+			line(playerX, playerY - halfPlayer, playerX, 0);
 		}
-	}	*/
+	}
+
+	public void draw() {
+		background(0);
+		drawPlayer(playerX, playerY, playerWidth);
+		
+	}
+
+		
 
 }
